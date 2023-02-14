2476d9f2c35df1c22f212640ab59b5f074009c5c
qismat <qismatnisha6243@gmail.com>
2023-02-14 20:45:36 +0530

    [qismat] Add :UC2-ValidationLastName

--- UserRegistrationProblem/UserRegistrationProblem/UserValidation.cs
+++ UserRegistrationProblem/UserRegistrationProblem/UserValidation.cs
@@ -9,11 +9,20 @@ namespace UserRegistrationProblem
 {
     public class UserValidation
     {
-        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{5}$";
+        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
         public void ValidationFirstName(string firstname)
         {
             Regex regex = new Regex(FIRST_NAME_REGEX);
             bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+        }
+        //UC2
+        //LastName the First Letter Should Capital and minimum 3 Character
+        public const string LASTNAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationLastName(string lastname)
+        {
+            Regex regex = new Regex(LASTNAME_REGEX);
+            bool result = regex.IsMatch(lastname);
             Console.WriteLine(result);
         }
     }
